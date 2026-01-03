17ffa43ba0716b11c20dabb02a1486f24b23f2b4
Kareem Mohamed <kareem.mohamed@adfinis.com>
2026-01-03 12:24:11 +0200

    Make build green

--- src/main/java/org/iti/app/Employee.java
+++ src/main/java/org/iti/app/Employee.java
@@ -27,92 +27,19 @@ public class Employee {
         this.isActive = isActive;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public double getSalary() {
         return salary;
     }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
     public boolean isActive() {
         return isActive;
     }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public LocalDate getHireDate() {
-        return hireDate;
-    }
-
-    public void setHireDate(LocalDate hireDate) {
-        this.hireDate = hireDate;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getDepartment() {
         return department;
     }
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+
 
 }
